7c09c4c3833b6109bb91e7add09fabacba1d8c31
nishanttaneja <nishant.taneja2701@gmail.com>
2021-01-06 15:28:35 +0530

    install Pod Firebase/Database & attach IBOutlets and IBActions

--- People/Controllers/EnrollPersonVC.swift
+++ People/Controllers/EnrollPersonVC.swift
@@ -7,7 +7,23 @@
 
 import UIKit
 
-class EnrollPersonVC: UIViewController {}
+class EnrollPersonVC: UIViewController {
+    // IBOutlets
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var dateOfBirthTextField: UITextField!
+    @IBOutlet weak var genderTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var hometownTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var telephoneNumberTextField: UITextField!
+    
+    // IBActions
+    @IBAction func selectProfilePhotoButtonPressed(_ sender: UIButton) {}
+    @IBAction func addUserButtonPressed(_ sender: UIButton) {}
+}
 
 //MARK:- Override View
 extension EnrollPersonVC {}
