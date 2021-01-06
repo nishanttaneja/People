a1efa3a67c6dc42215a3fd8bc4a689edbcbe9c74
nishanttaneja <nishant.taneja2701@gmail.com>
2021-01-06 13:34:43 +0530

    add DataModel Person

---
+++ People/Models/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  People
+//
+//  Created by Nishant Taneja on 06/01/21.
+//
+
+import UIKit
+
+struct Person {
+    let image: UIImage
+    let firstName: String
+    let lastName: String
+    let dateOfBirth: String
+    let gender: String
+    let country: String
+    let state: String
+    let hometown: String
+    let mobile: String
+    let telephone: String
+}
